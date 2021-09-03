6f45ebc143d277d85480c7b6089777297f567ca7
Delkys Welffer <delkys.yo@gmail.com>
2021-09-03 16:17:52 -0400

    Added URL for searching pokemons

--- poke_api/urls.py
+++ poke_api/urls.py
@@ -16,6 +16,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from pokemons.views import PokemonSearchAPIView
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('pokemons/search', PokemonSearchAPIView.as_view(), name="pokemon_search"),
 ]
