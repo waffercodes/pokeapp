62debcd290fc636c6aa5f884eb91741eaa510095
Delkys Welffer <delkys.yo@gmail.com>
2021-09-03 16:19:32 -0400

    Added APIView for searching Pokemons stored in the DB

--- pokemons/views.py
+++ pokemons/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
-# Create your views here.
+from pokemons.models import Pokemon, EvolutionChain
+from pokemons.serializers import PokemonSerializer
+
+
+class PokemonSearchAPIView(APIView):
+    """
+    Search Pokemon by name web service
+    """
+
+    def get(self, request, *args, **kwargs):
+        pokemon_name = request.query_params.get('pokemon_name', None)
+
+        pokemons = Pokemon.objects.filter(
+            name__icontains=pokemon_name
+        )
+        pokemons_serialized = PokemonSerializer(pokemons, many=True)
+
+        return Response(pokemons_serialized.data)
